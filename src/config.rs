6467c8141334c8b0cb3f0a760f07d8b36d290f53
Ole Kristian Sandum <oksandum@gmail.com>
2021-03-07 17:18:27 +0100

    Allow multiple alarm "objects"

--- src/config.rs
+++ src/config.rs
@@ -1,17 +1,34 @@
 use std::fs::File;
 
-use serde::Deserialize;
+use chrono::Weekday;
+use serde::de::Error;
+use serde::{Deserialize, Deserializer};
 
 #[derive(Deserialize)]
 pub struct Config {
-    pub alarm_time: AlarmTime,
+    pub alarms: Vec<Alarm>,
     pub audio_path: String,
 }
 
 #[derive(Deserialize, Debug)]
-pub struct AlarmTime {
+pub struct Alarm {
     pub hour: u8,
     pub minute: u8,
+    #[serde(deserialize_with = "deserialize_weekdays")]
+    pub weekdays: Vec<Weekday>,
+}
+
+fn deserialize_weekdays<'de, D>(deserializer: D) -> Result<Vec<Weekday>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    Vec::<String>::deserialize(deserializer)?
+        .into_iter()
+        .map(|s| {
+            s.parse()
+                .map_err(|_| D::Error::custom("unable to parse weekday"))
+        })
+        .collect()
 }
 
 pub fn read() -> anyhow::Result<Config> {
